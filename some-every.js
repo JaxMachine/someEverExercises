21d172bc5363dcd76ffa7de3e2643481d464321e
JaxMachine <newtradesofjack@gmail.com>
2024-02-02 15:22:53 -0600

    Project Finished

--- some-every.js
+++ some-every.js
@@ -1,74 +1,43 @@
-/*
-Write a function called hasOddNumber which accepts an array and returns true if the array contains at least one odd number, otherwise it returns false.
+//Has Odd Number Function
+function hasOddNumber(array) {
+  return array.some((val) => {
+    return val % 2 !== 0;
+  });
+}
 
-Examples:
-    hasOddNumber([1,2,2,2,2,2,4]) // true
-    hasOddNumber([2,2,2,2,2,4]) // false
-*/
+//Has a Zero
+function hasAZero(num) {
+  return num
+    .toString()
+    .split("")
+    .some((val) => {
+      return val === "0";
+    });
+}
 
-function hasOddNumber(arr) {}
+//Has only Odd Numbers
+function hasOnlyOddNumbers(array) {
+  return array.every((val) => {
+    return val % 2 !== 0;
+  });
+}
 
-/*
-Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
+//Has No Duplicates
+function hasNoDuplicates(array) {
+  return array.every((val) => {
+    return array.indexOf(val) === array.lastIndexOf(val);
+  });
+}
 
-Examples:
-    hasAZero(3332123213101232321) // true
-    hasAZero(1212121) // false
-*/
-
-function hasAZero(num) {}
-
-/*
-Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
-
-Examples:
-    hasOnlyOddNumbers([1,3,5,7]) // true
-    hasOnlyOddNumbers([1,2,3,5,7]) // false
-*/
-
-function hasOnlyOddNumbers(arr) {}
-
-/*
-Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
-
-Examples:
-    hasNoDuplicates([1,2,3,1]) // false
-    hasNoDuplicates([1,2,3]) // true
-*/
-
-function hasNoDuplicates(arr) {}
-
-/*
-Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
-
-Examples:
-    var arr = [
-        {title: "Instructor", first: 'Elie', last:"Schoppik"}, 
-        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true}, 
-        {title: "Instructor", first: 'Matt', last:"Lane"}, 
-        {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
-    ]
-    
-    hasCertainKey(arr,'first') // true
-    hasCertainKey(arr,'isCatOwner') // false
-*/
-
-function hasCertainKey(arr, key) {}
-
-/*
-Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
-
-Examples:
-    var arr = [
-        {title: "Instructor", first: 'Elie', last:"Schoppik"}, 
-        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true}, 
-        {title: "Instructor", first: 'Matt', last:"Lane"}, 
-        {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
-    ]
-    
-    hasCertainValue(arr,'title','Instructor') // true
-    hasCertainValue(arr,'first','Elie') // false
-    
-*/
-
-function hasCertainValue(arr, key, searchValue) {}
+//Has Certain Key
+function hasCertainKey(array, key) {
+  return array.every((val) => {
+    return key in val;
+  });
+}
+//Has certain Value
+function hasCertainValue(array, key, searchValue) {
+  return array.every((val) => {
+    return val[key] === searchValue;
+  });
+}
